53f193b253326dd16cad53f988feb67b5a744f12
rahulpatel-tavant <rahul.patel@tavant.com>
2019-02-27 21:30:16 +0530

    Two Fer Program in GO

---
+++ two-fer/two_fer.go
@@ -0,0 +1,17 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package twofer should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package twofer
+
+import "strings"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+  result := "One for you, one for me."
+  if(len(name) > 0){
+    result = strings.Replace(result, "you", name, 1)
+  }
+	return result
+}
